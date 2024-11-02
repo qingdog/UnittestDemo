d49a0509be79608d0133e16248958da4f45f816d
qingdog <qingdoor@gmail.com>
2024-11-02 17:00:17 +0800

    ui-auto2

--- utils/api/googleapis_translate_api.py
+++ utils/api/googleapis_translate_api.py
@@ -3,8 +3,7 @@ import requests
 
 def translate(q='hello world'):
     headers = {'Host': 'translate.googleapis.com'}
-    # res = requests.get(f'http://142.250.0.161/translate_a/single?client=at&sl=en&tl=zh-CN&dt=t&q={q}', headers=headers)
-    res = requests.get(f'https://translate-idea.pages.dev/translate_a/single?client=at&sl=en&tl=zh-CN&dt=t&q={q}', verify=False)
+    res = requests.get(f'http://142.250.0.161/translate_a/single?client=at&sl=en&tl=zh-CN&dt=t&q={q}', headers=headers)
     # res = requests.get('https://142.250.0.161/translate_a/single?client=at&sl=en&tl=zh-CN&dt=t&q=%s' % q,
     # headers={'Host': 'translate.googleapis.com'}, verify=False)
     return res.text
